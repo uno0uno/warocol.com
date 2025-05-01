94d251c2e006007df8c2adf28135b65a53dc96e9
uno0uno <anderson.electronico@gmail.com>
2025-05-01 02:06:13 -0500

    chamge name baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     apiKey: process.env.NUXT_API_KEY || '',
     public: {
-      baseUrl: process.env.NUXT_BASE_URL || 'https://warolabs.com'
+      baseUrl: process.env.NUXT_BASE_URL || 'https://warocol.com'
     }
   },
   app: {
@@ -29,7 +29,7 @@ export default defineNuxtConfig({
     '@nuxtjs/robots'
   ],
   site: {
-    url: 'https://warolabs.com'
+    url: 'https://warocol.com'
   },
   robots: {
     credits: false,
